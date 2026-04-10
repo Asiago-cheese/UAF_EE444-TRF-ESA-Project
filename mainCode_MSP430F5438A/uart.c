1b55c9ab641b2aad229d14eb3aa8b62f2f2c9f66
Asiago-cheese <wruiz@alaska.edu>
2026-04-10 10:41:33 -0800

    Update uart.c
    Mismatch of UART perifpheral. Some lines called for UCA0xxx and others for UCA1xxx; changed all to UCA1xxx

--- mainCode_MSP430F5438A/uart.c
+++ mainCode_MSP430F5438A/uart.c
@@ -10,10 +10,10 @@ void initUART(void)
     UCA1CTL0 = UCPEN + UCPAR;	//even parity, 8 data bits, 1 stop bit
     //Divider for Clock:
     //oversampling on, reccommended values from table 36-5
-    UCA0BR0 = 8;		//lower 8 bits
-    UCA0BR1 = 0;			//upper 8 bits
-    UCA0MCTL = UCBRS_0 | UCBRF_11 | UCOS16;
-    UCA0CTL1 &= ~UCSWRST; 		//Enable UART
+    UCA1BR0 = 8;		//lower 8 bits
+    UCA1BR1 = 0;			//upper 8 bits
+    UCA1MCTL = UCBRS_0 | UCBRF_11 | UCOS16;
+    UCA1CTL1 &= ~UCSWRST; 		//Enable UART
 }
 
 
@@ -21,8 +21,8 @@ void initUART(void)
 //UART transmit functions
 void uartSendChar(char c)
 {
-    while (!(UCA0IFG & UCTXIFG));
-    UCA0TXBUF = c;
+    while (!(UCA1IFG & UCTXIFG));
+    UCA1TXBUF = c;
 }
 void uartSendString(const char *str)
 {
